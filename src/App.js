799e6426604bacf75ccc1c9eedf705e81f214ae0
Nitin Kumar <nitinrishab2000@gmail.com>
2025-09-11 00:42:59 +0530

    TicketList Created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import TicketForm from "./components/TicketForm";
 import { useReducer } from "react";
 import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
 
 function App() {
   const initialState = { tickets: [] };
@@ -14,6 +15,15 @@ function App() {
       <div class="container">
         <h1>Welcome to Bug Blaster</h1>
         <TicketForm dispatch={dispatch}></TicketForm>
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+            <TicketList
+              tickets={state.tickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
       </div>
     </div>
   );
